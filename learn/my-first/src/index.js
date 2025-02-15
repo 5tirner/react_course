03de5b7d0163cbd6f1e36036eb2ab84132d22fde
5tirner <thatfor1337@gmail.com>
2025-02-15 15:35:01 +0100

    Continue

---
+++ learn/my-first/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const elm = <h1>Hello World!</h1>
+const ROOT = ReactDOM.createRoot(document.getElementById('root'));
+ROOT.render(elm);
